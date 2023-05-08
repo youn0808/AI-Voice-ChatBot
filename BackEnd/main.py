7cecae6b6b30944c396ccdf62a75bfb2deed160b
seung <hhwannn2@gmail.com>
2023-05-08 12:10:00 -0500

    setup env & backend api

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+# uvicorn main:app
+# source venv/bin/activate
+# uvicorn main:app --reload
 
+# Main Imports
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+# Custom Functions Impoprts
+# ....
+
+# Initiate App
 app = FastAPI()
 
+# CORS - Origins
+origins = [
+    "http://localhost:3030",
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# cors - Middleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/health")
+async def check_health():
+    return {"message": "healthy"}
