6d68ebf99f1f96923dea69dfeb4319317a5dfbc6
seung <hhwannn2@gmail.com>
2023-06-04 19:44:00 -0500

    call record audio

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -5,13 +5,19 @@ function Controller() {
   const [isLoading, setIsLoading] = useState(false);
   const [messages, setMessages] = useState<any[]>([]);
 
+  const [blob, setBlob] = useState("");
+
   const createBlobUrl = (data: any) => {};
-  const handleStop = async () => {};
+  const handleStop = async (blobUrl: string) => {
+    console.log(blobUrl);
+    setBlob(blobUrl);
+  };
 
   return (
     <div className="h-screen overflow-y-hidden">
       <Title setMessages={setMessages} />
       <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
+        <audio src={blob} controls />
         {/* {Recorder} */}
         <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-blue-600 to-green-200 text-white">
           <div className="flex justify-center items-center w-full">
