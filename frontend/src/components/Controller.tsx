2c20854382d15c850d3b2b0367536d1e07b4a03c
seung <hhwannn2@gmail.com>
2023-06-07 11:44:00 -0500

    append new recorded message to message array

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -7,17 +7,26 @@ function Controller() {
 
   const [blob, setBlob] = useState("");
 
-  const createBlobUrl = (data: any) => {};
+  const createBlobUrl = (data: any) => {
+    const blob = new Blob([data], { type: "audio/mpeg" });
+    const url = window.URL.createObjectURL(blob);
+    return url;
+  };
+
   const handleStop = async (blobUrl: string) => {
-    console.log(blobUrl);
-    setBlob(blobUrl);
+    setIsLoading(true);
+
+    //Append recorded new message to messages
+    const myMessage = { sender: "me", blobUrl };
+    const messagesArr = [...messages, myMessage];
+
+    setIsLoading(false);
   };
 
   return (
     <div className="h-screen overflow-y-hidden">
       <Title setMessages={setMessages} />
       <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
-        <audio src={blob} controls />
         {/* {Recorder} */}
         <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-blue-600 to-green-200 text-white">
           <div className="flex justify-center items-center w-full">
