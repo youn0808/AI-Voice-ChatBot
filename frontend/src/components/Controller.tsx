fd2b31e1d73221211ad437ad8bfa67952ac18491
seung <hhwannn2@gmail.com>
2023-06-09 12:14:00 -0500

    imp conversation div

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -63,6 +63,12 @@ function Controller() {
     <div className="h-screen overflow-y-hidden">
       <Title setMessages={setMessages} />
       <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
+        {/* Conversation */}
+        <div className="mt-5 px-5">
+          {messages?.map((audio, index) => {
+            return <div></div>;
+          })}
+        </div>
         {/* {Recorder} */}
         <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-blue-600 to-green-200 text-white">
           <div className="flex justify-center items-center w-full">
