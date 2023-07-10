d1b36ff2b89eba6bf5c31a2f3435c44c3d72a233
seung <hhwannn2@gmail.com>
2023-07-10 12:21:00 -0500

    fix record msg component

--- frontend/src/components/RecordMsg.tsx
+++ frontend/src/components/RecordMsg.tsx
@@ -1,15 +1,19 @@
 import { ReactMediaRecorder } from "react-media-recorder";
 import RecordIcon from "./RecordIcon";
 
-type Props = {
-  handleStop: any;
+// Define a specific type for the handleStop function
+type HandleStopFunction = (blobUrl: string) => void;
+
+type RecordMsgProps = {
+  handleStop: HandleStopFunction; // Use the specific type
 };
 
-function RecordMsg({ handleStop }: Props) {
+// Functional component to render the recording message and controls
+function RecordMsg({ handleStop }: RecordMsgProps) {
   return (
     <ReactMediaRecorder
-      audio
-      onStop={handleStop}
+      audio // Configure ReactMediaRecorder for audio recording
+      onStop={handleStop} // Pass the handleStop function to be executed on stop
       render={({ status, startRecording, stopRecording }) => (
         <div className="mt-1">
           <button
